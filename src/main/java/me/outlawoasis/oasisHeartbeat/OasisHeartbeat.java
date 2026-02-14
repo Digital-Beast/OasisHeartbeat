5d29cfac06c2ed5229d7d449bda378289513228a
BuildTools <unconfigured@null.spigotmc.org>
2026-02-14 01:39:11 +0100

    Initial commit – OasisHeartbeat setup

---
+++ src/main/java/me/outlawoasis/oasisHeartbeat/OasisHeartbeat.java
@@ -0,0 +1,17 @@
+package me.outlawoasis.oasisHeartbeat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class OasisHeartbeat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
